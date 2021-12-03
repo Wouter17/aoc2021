cef55429ce6f93d10e84e7da57b1f7f38386065a
Wouter17 <Wouterbreedveld17@gmail.com>
2021-12-03 20:38:18 +0100

    Added answers for day 2

--- src/day02/part1.ts
+++ src/day02/part1.ts
@@ -1,5 +1,15 @@
-import { parseInput } from '../util';
+import * as fs from 'fs';
 
-const input = parseInput();
+let text: any = fs.readFileSync('./src/day02/input.txt', 'utf-8');
+text = text.split(/\r?\n/);
 
-// TODO: Complete Part 1
+let depth = 0;
+let horizontal = 0;
+
+for (let i = 0; i < text.length; i++) {
+    if (text[i].startsWith('forward')) horizontal += Number(text[i].split(' ')[1]);
+    if (text[i].startsWith('up')) depth -= Number(text[i].split(' ')[1]);
+    if (text[i].startsWith('down')) depth += Number(text[i].split(' ')[1]);
+}
+
+export default depth * horizontal;
--- src/day02/part2.ts
+++ src/day02/part2.ts
@@ -1,5 +1,19 @@
-import { parseInput } from '../util';
+import * as fs from 'fs';
 
-const input = parseInput();
+let text: any = fs.readFileSync('./src/day02/input.txt', 'utf-8');
+text = text.split(/\r?\n/);
 
-// TODO: Complete Part 2
+let height = 0;
+let horizontal = 0;
+let aim = 0;
+
+for (let i = 0; i < text.length; i++) {
+    if (text[i].startsWith('forward')) {
+        horizontal += Number(text[i].split(' ')[1]);
+        height += aim * Number(text[i].split(' ')[1]);
+    }
+    if (text[i].startsWith('up')) aim -= Number(text[i].split(' ')[1]);
+    if (text[i].startsWith('down')) aim += Number(text[i].split(' ')[1]);
+}
+
+export default height * horizontal;
