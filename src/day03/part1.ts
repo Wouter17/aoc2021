9488afb3bd134ad1a69b74aee58f4072f1329448
Wouter17 <Wouterbreedveld17@gmail.com>
2021-12-03 15:59:01 +0100

    Updated git ignore, package, and uploaded solutions for day 1 and 3

--- src/day03/part1.ts
+++ src/day03/part1.ts
@@ -1,5 +1,20 @@
-import { parseInput } from '../util';
+import fs from 'fs';
 
-const input = parseInput();
+let text: any = fs.readFileSync('./values.txt', 'utf-8');
+text = text.split(/\r?\n/);
 
-// TODO: Complete Part 1
+let gamma: number[] = [];
+
+for (let i = 0; i < text.length; i++) {
+    for (let j = 0; j < text[i].length; j++) {
+        if (Number(text[i].charAt(j)) === 1)
+            gamma[j] ? (gamma[j] = gamma[j] + 1) : (gamma[j] = 1);
+    }
+}
+
+gamma = gamma.map((value) => (value > 500 ? 1 : 0));
+const gammaResult: number = parseInt(gamma.join(''), 2);
+
+const epsilon = Math.pow(2, text[0].length) - 1 - gammaResult;
+
+console.log(epsilon * gammaResult);
