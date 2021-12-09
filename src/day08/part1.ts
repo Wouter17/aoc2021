1457557ff7edd36adac0671a63ab0a37b9e5e487
Wouter17 <Wouterbreedveld17@gmail.com>
2021-12-09 18:37:37 +0100

    Added first solution for day 8

--- src/day08/part1.ts
+++ src/day08/part1.ts
@@ -1,5 +1,19 @@
-import { parseInput } from '../util';
+import * as fs from 'fs';
 
-const input = parseInput();
+let text: any = fs.readFileSync('./src/day08/input.txt', 'utf-8');
+text = text.split(/\r?\n/).map((element: string) => element.split(' | '));
+text = text.map((element: string[]) => element.map((value: string) => value.split(' ')));
 
-// TODO: Complete Part 1
+const count: number[] = text.map((element: string[][]) =>
+    element[1].reduce((previousValue, currentValue) => {
+        if ([2, 4, 3, 7].indexOf(currentValue.length) > -1) {
+            return previousValue + 1;
+        }
+        return previousValue;
+    }, 0)
+);
+
+export default count.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+);
