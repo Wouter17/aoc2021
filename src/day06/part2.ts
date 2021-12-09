f7d84c854c224e59b7e08613b963a8654a490d3f
Wouter17 <Wouterbreedveld17@gmail.com>
2021-12-09 18:37:15 +0100

    Added solutions for day 6

--- src/day06/part2.ts
+++ src/day06/part2.ts
@@ -1,5 +1,24 @@
-import { parseInput } from '../util';
+import * as fs from 'fs';
 
-const input = parseInput();
+const text: string = fs.readFileSync('./src/day06/input.txt', 'utf-8');
+const startingFish: number[] = text.split(',').map(Number);
 
-// TODO: Complete Part 2
+const fish = new Array(9).fill(0);
+
+startingFish.forEach((element) => {
+    fish[element]++;
+});
+
+for (let i = 0; i < 256; i++) {
+    const tmp = fish[0];
+    for (let j = 0; j < fish.length - 1; j++) {
+        fish[j] = fish[j + 1];
+    }
+    fish[6] += tmp;
+    fish[8] = tmp;
+}
+
+export default fish.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+);
