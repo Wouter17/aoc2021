9acbc8f05ceb9d1fd7d60b53578bb75f1f29bb85
Wouter17 <Wouterbreedveld17@gmail.com>
2021-12-09 18:37:53 +0100

    Added first solution for day 9

--- src/day09/part1.ts
+++ src/day09/part1.ts
@@ -1,5 +1,30 @@
-import { parseInput } from '../util';
+import * as fs from 'fs';
 
-const input = parseInput();
+let text: any = fs.readFileSync('./src/day09/input.txt', 'utf-8');
+text = text.split(/\r?\n/);
+text = text.map((element: string) => element.split('').map(Number));
 
-// TODO: Complete Part 1
+const lowpoints = [];
+
+for (let i = 0; i < text.length; i++) {
+    for (let j = 0; j < text[i].length; j++) {
+        if (
+            text[i][j] <
+                (text[i - 1] === undefined || text[i - 1][j] === undefined
+                    ? 10
+                    : text[i - 1][j]) &&
+            text[i][j] <
+                (text[i + 1] === undefined || text[i + 1][j] === undefined
+                    ? 10
+                    : text[i + 1][j]) &&
+            text[i][j] < (text[i][j - 1] !== undefined ? text[i][j - 1] : 10) &&
+            text[i][j] < (text[i][j + 1] !== undefined ? text[i][j + 1] : 10)
+        )
+            lowpoints.push(text[i][j]);
+    }
+}
+
+export default lowpoints.reduce(
+    (previousValue, currentValue) => previousValue + currentValue + 1,
+    0
+);
